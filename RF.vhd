665b29e843deee39264147ead9720e48db9ed58c
Juan patino Hernandez <juan.patino@utp.edu.co>
2016-10-16 22:41:25 -0500

    se carga unidad de control
    unidad de control y se prueba

---
+++ RF.vhd
@@ -0,0 +1,70 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    17:35:31 10/16/2016 
+-- Design Name: 
+-- Module Name:    RF - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+Library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity registroArchivo_RF is
+    Port ( rs1 : in  STD_LOGIC_VECTOR (4 downto 0);
+           rs2 : in  STD_LOGIC_VECTOR (4 downto 0);
+           reset : in  STD_LOGIC;
+           crs1 : out  STD_LOGIC_VECTOR (31 downto 0);
+           crs2 : out  STD_LOGIC_VECTOR (31 downto 0);
+			  crd : in  STD_LOGIC_VECTOR (31 downto 0);
+           rd : in  STD_LOGIC_VECTOR (4 downto 0));
+end registroArchivo_RF;
+
+architecture Behavioral of registroArchivo_RF is
+
+type ram_type is array (0 to 39) of std_logic_vector (31 downto 0);
+	signal reg : ram_type :=(others => x"00000000");
+
+begin
+process(rs1,rs2,reset, rd,crd)
+begin
+	if(reset = '1')then
+				crs1 <= (others=>'0');
+				crs2 <= (others=>'0');
+			
+				reg <= (others => x"00000000");
+			else
+				crs1 <= reg(conv_integer(rs1));
+				crs2 <= reg(conv_integer(rs1));
+				
+				
+			if(rd  /= "000000")then
+					reg(conv_integer(rd)) <= crd;
+				end if;
+			end if;
+	
+end process; 
+
+end Behavioral;
+
